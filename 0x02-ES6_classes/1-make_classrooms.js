df456d7dce88452119776edf4c7b024cc43d3054
isybliss <isyblissumunna@gmail.com>
2023-06-30 04:01:47 +0000

    applying getter and setter method

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import Classroom from './0-classroom';
+
+export default function initializeRooms() {
+  const arr = [];
+  
+  arr.push(new Classroom(19));
+  arr.push(new Classroom(20));
+  arr.push(new Classroom(34));
+
+  return arr;
+}
