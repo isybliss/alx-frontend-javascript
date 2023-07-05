774f560a61faa1fb862e121c0171b4ad780aa9c0
isybliss <isyblissumunna@gmail.com>
2023-07-05 16:17:46 +0100

    task 1: adding class interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,16 +7,60 @@ interface Teacher {
   [key: string]: any;
 }
 
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const teacher3: Directors = {
+const director1: Directors = {
   firstName: 'John',
-  fullTimeEmployee: true,
   lastName: 'Doe',
   location: 'London',
+  fullTimeEmployee: true,
   numberOfReports: 17,
 };
 
-console.log(teacher3);
+console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstLetter = firstName.charAt(0);
+  return `${firstLetter}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe"));
+
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string{
+    return this.firstName;
+  }
+}
+
